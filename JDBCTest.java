346aec2559695569a1bd80f849e975ed1f1a6e09
ifiroj <firoj.acharya@gmail.com>
2021-11-25 10:11:54 +0545

    db file updated

--- JDBCTest.java
+++ JDBCTest.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class JDBCTest
 {
-	Connection con;
+	Connection con; // for connection
 	Statement s; // for SELECT
 	PreparedStatement ps; // for INSERT, UPDATE, DELETE
 	ResultSet rs;
@@ -12,14 +12,13 @@ public class JDBCTest
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver"); // loading the driver
-			con = DriverManager.getConnection("jdbc:mysql://localhost/javaclass?user=root&password="); // establishing the connection	
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/javaclass?user=root&password="); // establishing the connection 
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.toString());
 		}		
 	}
-
 
 	public void closeConnection()
 	{
@@ -38,7 +37,7 @@ public class JDBCTest
 		try
 		{
 			s = con.createStatement();
-			rs = s.executeQuery("SELECT * FROM javaclass");
+			rs = s.executeQuery("SELECT * FROM records");
 
 			while(rs.next())
 			{
@@ -55,68 +54,58 @@ public class JDBCTest
 		}
 	}
 
-
 	public void insertData()
 	{
-
 		try
 		{
-			ps = con.prepareStatement("INSERT INTO javaclass(roll_no, name, email, address) VALUES(?, ?, ?, ?)");
+			ps = con.prepareStatement("INSERT INTO records(name, address, email, roll_name) VALUES(?, ?, ?, ?)");
 
-			ps.setString(2, "Sita");
-			ps.setString(4, "dharan");
-			ps.setString(3, "Sita@w.com");
-			ps.setInt(1, 33);
+			ps.setString(1, "John Cena");
+			ps.setString(2, "New York");
+			ps.setString(3, "demo@email.com");
+			ps.setInt(4, 33);
 
 			ps.executeUpdate();
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.toString());
-		}
-		
+		}	
 	}
-
-
 
 	public void updateData()
 	{
-
 		try
 		{
-			ps = con.prepareStatement("UPDATE javaclass SET name = ?, email = ?, roll_no = ?, address = ? WHERE id = ?");
+			ps = con.prepareStatement("UPDATE records SET name = ?, address = ?, email = ?, roll_no = ? WHERE id = ?");
 
-			ps.setString(1, "Harry");
-			ps.setString(2, "harry@w.com");
-			ps.setInt(3, 330);
-			ps.setString(4, "dhankuta");
-			ps.setInt(5, 2);
+			ps.setString(1, "Jeff Hardy");
+			ps.setString(2, "London");
+			ps.setString(3, "demo@email.com");
+			ps.setInt(4, 23);
+			ps.setInt(5, 3);
 
 			ps.executeUpdate();
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.toString());
-		}
-		
+		}	
 	}
-
 
 	public void deleteData()
 	{
-
 		try
 		{
-			ps = con.prepareStatement("DELETE FROM javaclass WHERE id = ?");
-			ps.setInt(1, 2);
+			ps = con.prepareStatement("DELETE FROM records WHERE id = ?");
+			ps.setInt(1, 3);
 
 			ps.executeUpdate();
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.toString());
-		}
-		
+		}	
 	}
 
 
@@ -124,11 +113,11 @@ public class JDBCTest
 	{
 		JDBCTest j = new JDBCTest();
 
-		//j.insertData();
+		j.insertData();
 		//j.updateData();
 		//j.deleteData();
 
-		//j.selectData();
+		j.selectData();
 
 		j.closeConnection();
 	}
